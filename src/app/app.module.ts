53bbd3333fcbc0642c1340edb20d41bc46eebe59
Lawrence Moore <lawrence2978@hotmail.com>
2017-10-08 09:44:19 -0400

    Redid routing module and 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing/routing.module';
 import { SearchComponent } from './search/search.component';
 import { RegisterComponent } from './register/register.component';
-
-const appRoutes: Routes = [
-    {path: 'search', component: SearchComponent},
-    {path: 'register', component: RegisterComponent}
-]
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-        appRoutes,
-        {enableTracing: true}
-    )
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SearchComponent,
+        RegisterComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        RoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
