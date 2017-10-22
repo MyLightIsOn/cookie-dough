27a8c7b7710cad912e68990a5d2746a90eb77ac4
Lawrence Moore <lawrence2978@hotmail.com>
2017-10-22 11:46:39 -0400

    Added nav menu
    The nav menu for mobile now works. Glyphicons were also added
    for icons. Menu animates as well.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    wasClicked = false;
+
+    /*Toggles the wasClicked property that adds a class to the elements with this property*/
+    menuToggle() {
+        this.wasClicked = !this.wasClicked;
+    }
 }
