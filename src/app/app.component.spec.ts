1090ad34cc8779108469232fa8fb72ca856d5672
Lawrence Moore <lawrence2978@hotmail.com>
2017-11-20 19:12:02 -0500

    Added unit test for http request

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,35 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { CompanyService} from './companies/company.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MockBackend } from '@angular/http/testing';
+
+import { CompanyService} from './companies/company.service';
+import { environment} from '../environments/environment';
+import {XHRBackend, Response, ResponseOptions } from '@angular/http';
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'companyObject'
+})
+export class MockPipe implements PipeTransform {
+
+    transform(value: any, args?: any): any {
+        return value;
+    }
+}
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
+      declarations: [ AppComponent, MockPipe ],
         imports: [ RouterTestingModule, HttpClientTestingModule ],
-        providers: [ CompanyService ]
+        providers: [ CompanyService, MockPipe,
+            { provide: environment.apiUrl, useValue: 'http://example.com'},
+            { provide: XHRBackend, useClass: MockBackend}
+        ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,8 +38,29 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-    it('should get companies', inject(CompanyService, (companyService: CompanyService) => {
-        expect(companyService.getCompanies).toBe('data');
-    }));
+  it('should return an Observable using a pipe', inject([CompanyService, XHRBackend, MockPipe], (companySerivce, mockBackend) => {
+      const mockResponse = {
+          data: [
+              { id: 0, name: 'Company 1' },
+              { id: 1, name: 'Company 2' },
+              { id: 2, name: 'Company 3' },
+              { id: 3, name: 'Company 4' },
+          ]
+      };
+
+      mockBackend.connections.subscribe((connection) => {
+          connection.mockRespond(new Response(new ResponseOptions({
+              body: JSON.stringify(mockResponse)
+          })));
+      });
+
+      companySerivce.getAllCompanies().subscribe((company) => {
+          expect(company.length).toBe(4);
+          expect(company[0].name).toEqual('Company 1');
+          expect(company[1].name).toEqual('Company 2');
+          expect(company[2].name).toEqual('Company 3');
+          expect(company[3].name).toEqual('Company 4');
+      });
+  }));
 });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { CompanyService } from './companies/company.service';
 import { Company } from './_interfaces/companies';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app-root',
@@ -21,8 +21,6 @@ export class AppComponent implements OnInit {
 
     /*Calls GET request for company list as soon as app loads*/
     ngOnInit() {
-        this.companyService.getAllCompanies().subscribe((data) => {
-           this.companies$ = data[0].records;
-        });
+        this.companyService.getAllCompanies().subscribe((data) => this.companies$ = data[0].records);
     }
 }
