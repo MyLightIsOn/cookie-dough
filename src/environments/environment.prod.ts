02050f2fdf211d18a04f75634c3e99b0540aff5f
Lawrence Moore <lawrence2978@hotmail.com>
2017-11-25 07:33:36 -0500

    Removed old API reference

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://gempathy-api.herokuapp.com',
     'API_ID_HEADER' : 'API_ID_HEADER',
     'API_ID' : 'API_ID',
     'API_KEY' : 'API_KEY',
