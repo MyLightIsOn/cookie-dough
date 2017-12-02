7f9768dc5cbf5fedb33fabbe3f16c5b449875e23
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-02 11:12:43 -0500

    Added tests for search component

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,15 +1,12 @@
-import { async, inject, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MockBackend } from '@angular/http/testing';
 import { FormsModule } from '@angular/forms';
-import { XHRBackend, Response, ResponseOptions } from '@angular/http';
 import { Component, Input } from '@angular/core';
 
 import { SearchComponent } from './search.component';
 import { CompaniesService } from '../companies/companies.service';
 import { ICompany } from '../_interfaces/companies';
-import { environment} from '../../environments/environment';
 
 
 import { Pipe, PipeTransform } from '@angular/core';
@@ -29,20 +26,8 @@ export class MockPipe implements PipeTransform {
 })
 export class MockFilterPipe implements PipeTransform {
 
-    transform(items: any, term: any): any {
-        if (term === undefined) { return items; }
-
-        return items.filter(function(item) {
-            for (const property in item) {
-                if (item[property] === null) {
-                    continue;
-                }
-                if (item[property].toString().toLowerCase().includes(term.toLowerCase())) {
-                    return true;
-                }
-            }
-            return false;
-        });
+    transform(value: any, args?: any): any {
+        return value;
     }
 }
 
@@ -55,48 +40,26 @@ class MockSearchResultsComponent {
     @Input() company: ICompany;
 }
 
-
 describe('SearchComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ SearchComponent, MockPipe, MockFilterPipe, MockSearchResultsComponent ],
             imports: [ RouterTestingModule, HttpClientTestingModule, FormsModule ],
-            providers: [ CompaniesService, MockPipe, MockFilterPipe,
-                { provide: environment['BASEURL'], useValue: 'http://example.com'},
-                { provide: XHRBackend, useClass: MockBackend}
-            ]
+            providers: [ CompaniesService, MockPipe, MockFilterPipe ]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(SearchComponent);
-        const mockSearchResuls = TestBed.createComponent(MockSearchResultsComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
 
-    it('should return an Observable using a pipe', inject([CompaniesService, XHRBackend, MockPipe,
-        MockFilterPipe], (companySerivce, mockBackend) => {
-        const mockResponse = {
-            data: [
-                { id: 0, name: 'Company 1' },
-                { id: 1, name: 'Company 2' },
-                { id: 2, name: 'Company 3' },
-                { id: 3, name: 'Company 4' },
-            ]
-        };
+    it('should call subscribeToCompanyData when created', async(() => {
+        const fixture = TestBed.createComponent(SearchComponent);
+        const app = fixture.debugElement.componentInstance;
+        spyOn(app, 'subscribeToCompanyData');
 
-        mockBackend.connections.subscribe((connection) => {
-            connection.mockRespond(new Response(new ResponseOptions({
-                body: JSON.stringify(mockResponse)
-            })));
-        });
-
-        companySerivce.getAllCompanies().subscribe((company) => {
-            expect(company.length).toBe(4);
-            expect(company[0].name).toEqual('Company 1');
-            expect(company[1].name).toEqual('Company 2');
-            expect(company[2].name).toEqual('Company 3');
-            expect(company[3].name).toEqual('Company 4');
-        });
+        app.ngOnInit();
+        expect(app.subscribeToCompanyData).toHaveBeenCalled();
     }));
 });
