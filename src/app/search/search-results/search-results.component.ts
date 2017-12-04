9f555f5c651ffa7404a8003446432f0690191a38
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-03 19:35:17 -0500

    Added pagination for search results.

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -1,19 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { ICompany } from '../../_interfaces/companies';
+import { PaginationService } from '../../pagination/pagination.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
+  providers: [ PaginationService ]
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent implements OnChanges {
   @Input() companies$;
   @Input() company: ICompany;
   @Input() searchSubmitted;
 
-  constructor() { }
+  constructor(private paginationService: PaginationService) { }
 
-  ngOnInit() {
+    private companiesArray;
+    private pager: any = {};
+
+  ngOnChanges() {
+    this.setPage(1);
+  }
+
+  // Create pagination based on the number of items in the array
+  setPage(page: number) {
+      if (this.companies$) {
+          /*this.allItems = this.companies$;*/
+          if (page < 1 || page > this.pager.totalPages) {
+              return;
+          }
+
+          // get pager object from service
+          this.pager = this.paginationService.getPager(this.companies$.length, page);
+
+          // get current page of items
+          this.companiesArray = this.companies$.slice(this.pager.startIndex, this.pager.endIndex + 1);
+      }
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CompaniesService } from '../companies/companies.service';
 
+import { CompaniesService } from '../companies/companies.service';
 import { ICompany } from '../_interfaces/companies';
 
 @Component({
