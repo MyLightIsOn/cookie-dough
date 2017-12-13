0f35f5d765e11d828f1c599cc3020850f482d68a
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-13 06:21:23 -0500

    Removed tutorial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
-import { RoutingModule } from './routing/routing.module';
+import { RoutingModule } from './routing.module';
 import { CompanyModule } from './companies/companies.module';
 import { SearchModule } from './search/search.module';
 import { LoginComponent } from './login/login.component';
@@ -13,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 import { DialogService } from './dialogs/dialog.service';
 import { FilterCompaniesPipe } from './_pipes/filter-companies.pipe';
@@ -24,7 +23,6 @@ import { FilterCompaniesPipe } from './_pipes/filter-companies.pipe';
         AppComponent,
         RegisterComponent,
         PageNotFoundComponent,
-        ComposeMessageComponent,
         LoginComponent,
         FilterCompaniesPipe
     ],
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SearchComponent } from '../search/search.component';
-import { RegisterComponent } from '../register/register.component';
-import { ComposeMessageComponent } from '../compose-message/compose-message.component';
-import { CanDeactivateGuard } from '../_guards/can-deactivate-guard.service';
-import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
-import { AuthGuard } from '../_guards/auth-guard.service';
+import { RegisterComponent } from './register/register.component';
+import { CanDeactivateGuard } from './_guards/can-deactivate-guard.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './_guards/auth-guard.service';
 
 const appRoutes: Routes = [
     {path: 'register', component: RegisterComponent},
-    {path: 'compose', component: ComposeMessageComponent, outlet: 'popup'},
     {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule', canLoad: [AuthGuard]},
     {path: '',   redirectTo: '/search', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent}
