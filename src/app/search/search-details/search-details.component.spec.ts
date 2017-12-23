f6b5a5d8f8de5b2a467bd2b6d30a6a4fcc3db136
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-23 11:49:05 -0500

    Updated Search Details tests

--- src/app/search/search-details/search-details.component.spec.ts
+++ src/app/search/search-details/search-details.component.spec.ts
@@ -1,25 +1,125 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { SearchDetailsComponent } from './search-details.component';
+import { CompaniesService } from '../../companies/companies.service';
+
+
+
+const mockCompanyService = {
+    startAtDetails: true,
+    companyDataObservable: {},
+    setFlag: jasmine.createSpy('setFlag'),
+    setSocialMedia: jasmine.createSpy('setSocialMedia')
+};
+
+const company = {
+    id: 0,
+    field_7: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+    ' Nullam vel neque eget enim tempus cursus. Curabitur porta ' +
+    'velit consequat pulvinar dignissim. Lorem ipsum dolor ' +
+    'sit amet',
+    field_4_raw: {
+        country: 'HK'
+    },
+    field_5_raw: {
+        thumb_url: ''
+    },
+    field_14_raw: '',
+    field_15_raw: '',
+    field_16_raw: ''
+};
+
+const route = {
+    snapshot: {
+        data: {
+            company: company
+        }
+    }
+};
+
+const router = {
+    navigate: jasmine.createSpy('navigate')
+};
 
 describe('SearchDetailsComponent', () => {
-  let component: SearchDetailsComponent;
-  let fixture: ComponentFixture<SearchDetailsComponent>;
+    let component: SearchDetailsComponent;
+    let fixture: ComponentFixture<SearchDetailsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchDetailsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ RouterTestingModule, HttpClientModule ],
+            declarations: [ SearchDetailsComponent ],
+            providers: [ CompaniesService,
+                { provide: CompaniesService, useValue: mockCompanyService },
+                { provide: ActivatedRoute, useValue: route },
+                { provide: Router, useValue: router }
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchDetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set up company to display', () => {
+        spyOn(component, 'truncateDescription');
+
+        component.ngOnInit();
+        expect(component.truncateDescription).toHaveBeenCalledWith(company['field_7']);
+        expect(mockCompanyService.setFlag).toHaveBeenCalledWith(company['field_4_raw']['country']);
+        expect(mockCompanyService.setFlag).toHaveBeenCalledWith(company['field_4_raw']['country']);
+        expect(mockCompanyService.setSocialMedia).toHaveBeenCalledWith([
+            company['field_14_raw'],
+            company['field_15_raw'],
+            company['field_16_raw']]
+        );
+    });
+
+    it('should toggle the truncated text', () => {
+        component.truncatedText = true;
+        component.toggled = true;
+        component.toggleDescription();
+
+        expect(component.truncatedText).toBeFalsy();
+        expect(component.toggled).toBeFalsy();
+    });
+
+    it('should check for startAtDetails and route accordingly', () => {
+        component.closeDetails();
+        expect(router.navigate).toHaveBeenCalledWith(['/']);
+
+        component.closeDetails();
+        expect(router.navigate).toHaveBeenCalledWith(['/search-results']);
+    });
+
+    it('should truncate text if over 150 characters', () => {
+        const text = 'Lorem ipsum dolor sit amet, consectetur ' +
+            'adipiscing elit. Nullam vel neque eget enim tempus ' +
+            'cursus. Curabitur porta velit consequat pulvinar' +
+            ' dignissim. Lorem ipsum dolor sit amet';
+        const truncText = component.truncateDescription(company['field_7']);
+
+        expect(truncText.length).toBe(153);
+    });
+
+    it('should NOT truncate text if under 150 characters', () => {
+        component.toggled = true;
+        const text = 'Lorem ipsum dolor sit amet, consectetur ' +
+            'adipiscing elit. Nullam vel neque eget enim tempus ' +
+            'cursus';
+        const truncText = component.truncateDescription(text);
+
+        expect(truncText.length).toEqual(text.length);
+        expect(component.toggled).toBeFalsy();
+    });
 });
