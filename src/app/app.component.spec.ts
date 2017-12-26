a9c328981ab5a9fee7828f25fc69d93d7bb5474b
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-26 08:06:27 -0500

    Updated App component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,17 +5,26 @@ import { CompaniesService} from './companies/companies.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-        imports: [ RouterTestingModule, HttpClientTestingModule ],
-        providers: [ CompaniesService ]
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AppComponent ],
+            imports: [ RouterTestingModule, HttpClientTestingModule ],
+            providers: [ CompaniesService ]
+        }).compileComponents();
+    }));
+
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        app.ngOnInit();
+        expect(app).toBeTruthy();
+    }));
+
+    it('should return activated route data', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        const outlet = {activatedRouteData: { page: 1}};
+        expect(app.getPage(outlet)).toBe(1);
+    }));
 });
 
