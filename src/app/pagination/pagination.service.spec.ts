193770e71033f4a7dc793947fdbffc7127646fe9
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-26 07:28:09 -0500

    Updated Pagination tests

--- src/app/pagination/pagination.service.spec.ts
+++ src/app/pagination/pagination.service.spec.ts
@@ -3,13 +3,36 @@ import { TestBed, inject } from '@angular/core/testing';
 import { PaginationService } from './pagination.service';
 
 describe('PaginationService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PaginationService]
-    });
-  });
 
-  it('should be created', inject([PaginationService], (service: PaginationService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [PaginationService]
+        });
+    });
+
+    it('should be created', inject([PaginationService], (service: PaginationService) => {
+        expect(service).toBeTruthy();
+    }));
+
+
+    it('should show 10 pages',
+        inject([PaginationService], (service: PaginationService) => {
+            const results = service.getPager(120, 1, 10);
+
+            expect(results.pages.length).toBe(10);
+        }));
+
+    it('should show 10 pages',
+        inject([PaginationService], (service: PaginationService) => {
+            const results = service.getPager(120, 9, 10);
+
+            expect(results.pages.length).toBe(10);
+        }));
+
+    it('should show 10 pages',
+        inject([PaginationService], (service: PaginationService) => {
+            const results = service.getPager(120, 7, 10);
+
+            expect(results.pages.length).toBe(10);
+        }));
 });
