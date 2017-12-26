f690598077c8463289890a79004dfb6eae2c8068
Lawrence Moore <lawrence2978@hotmail.com>
2017-12-26 07:43:32 -0500

    Updated Filter Companies pipe tests

--- src/app/_pipes/filter-companies.pipe.spec.ts
+++ src/app/_pipes/filter-companies.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { FilterCompaniesPipe } from './filter-companies.pipe';
 
 describe('FilterCompaniesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterCompaniesPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new FilterCompaniesPipe();
+        expect(pipe).toBeTruthy();
+    });
+
+    it('return filtered items', () => {
+        const pipe = new FilterCompaniesPipe();
+        const mockCompany = [
+            { id: '0', field_3: 'company 1', field_4_raw: {}, field_29: true, field_32_raw: 0.1},
+            { id: '1', field_3: 'company 2', field_4_raw: {}, field_29: true, field_32_raw: 0.4},
+            { id: '2', field_3: 'company 3', field_4_raw: {}, field_29: true, field_32_raw: 0.5},
+            { id: '3', field_3: 'company 4', field_4_raw: {}, field_29: true, field_32_raw: 0.7},
+            { id: '4', field_3: 'company 5', field_4_raw: null, field_29: true, field_32_raw: 1},
+            { id: '5', field_3: 'company 6', field_4_raw: {}, field_29: true, field_32_raw: undefined}
+        ];
+
+        const result = pipe.transform(mockCompany, 'company 6');
+        expect(result[0]).toBe(mockCompany[5]);
+    });
 });
