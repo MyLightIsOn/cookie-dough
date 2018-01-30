37cca0256c7130b91ab5982097b04caf145049c1
Lawrence Moore <lawrence2978@hotmail.com>
2018-01-30 15:28:39 -0500

    Updated env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     'API_ID' : 'API_ID',
     'API_KEY' : 'API_KEY',
     'API_KEY_HEADER' : 'API_KEY_HEADER',
-    'BASEURL' : 'https://gentle-crag-11632.herokuapp.com'
+    'BASEURL' : 'https://obscure-journey-88126.herokuapp.com'
 };
 
