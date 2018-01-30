87436924793fc44e291a0481803bd912435df2d6
Lawrence Moore <lawrence2978@hotmail.com>
2018-01-30 15:15:20 -0500

    Fixed nav bug and set up profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { AuthGuard } from '../_guards/auth-guard.service';
 
 @Component({
     selector: 'app-profile',
@@ -10,10 +12,14 @@ import { ActivatedRoute } from '@angular/router';
 export class ProfileComponent implements OnInit {
     sessionId: Observable<string>;
     token: Observable<string>;
+    user: object;
+    firstName: string;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(private route: ActivatedRoute, public authGuard: AuthGuard) {}
 
     ngOnInit() {
+        this.user = this.authGuard.getSession();
+        this.firstName = this.user['user']['values']['name']['first'];
         /*// Capture the session ID if available
         this.sessionId = this.route
             .queryParamMap
