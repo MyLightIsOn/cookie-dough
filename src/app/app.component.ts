51e2a8755c216b35ed53a2ccb55beac77f5986a5
Lawrence Moore <lawrence2978@hotmail.com>
2018-02-13 09:23:09 -0500

    Added nav login for desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 import { CompaniesService } from './companies/companies.service';
 import { AuthService } from './auth.service';
 import { searchAnimations } from './_animations/searchAnimations';
@@ -12,8 +13,11 @@ import { searchAnimations } from './_animations/searchAnimations';
 
 
 export class AppComponent implements OnInit {
+    email: string;
+    password: string;
+    message: string;
 
-    constructor(private companyService: CompaniesService, public authService: AuthService) {}
+    constructor(private companyService: CompaniesService, public authService: AuthService, public router: Router) {}
 
     // Uses service to get a company list as soon as the app loads
     ngOnInit() {
@@ -21,11 +25,39 @@ export class AppComponent implements OnInit {
             this.authService.isLoggedIn = true;
         }
         this.companyService.getAllCompanies().subscribe();
+        console.log(window.innerWidth);
     }
 
     // returns the route for animations
     getPage(outlet) {
+        // console.log(outlet.activatedRouteData['page']);
         return outlet.activatedRouteData['page'];
+    }
+
+    setMessage() {
+        this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+    }
+
+    login() {
+        const navigationExtras: NavigationExtras = {
+            queryParamsHandling: 'preserve',
+            preserveFragment: true
+        };
+
+        this.message = 'Trying to log in ...';
+
+        this.authService.login(this.email, this.password).subscribe((res => {
+            this.setMessage();
+            this.authService.checkResponse(res);
+            if (this.authService.isLoggedIn) {
+                // Get the redirect URL from our auth service
+                // If no redirect has been set, use the default
+                const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/profile';
+
+                // Redirect the user
+                this.router.navigate([redirect], navigationExtras);
+            }
+        }));
     }
 
     logout() {
