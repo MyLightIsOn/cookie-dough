f41a3dbb7c921009dda1fe97daa436a876fbf0a7
Lawrence Moore <lawrence2978@hotmail.com>
2018-03-09 14:18:18 -0500

    Added registration layout and styles

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterComponent } from './register/register.component';
 import { CanDeactivateGuard } from './_guards/can-deactivate-guard.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-    {path: 'register', component: RegisterComponent},
     {path: '',   redirectTo: '/search', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent}
 ];
--- src/app/search/search-routing.module.ts
+++ src/app/search/search-routing.module.ts
@@ -5,11 +5,15 @@ import { SearchComponent } from './search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { SearchDetailsComponent } from './search-details/search-details.component';
 import { SearchResolverService } from './search-resolver.service';
+import { RegisterComponent } from '../register/register.component';
 
 import { LoginModule } from '../login/login.module';
 import { ProfileModule } from '../profile/profile.module';
 
 const searchRoutes: Routes = [
+    {
+        path: 'register', component: RegisterComponent
+    },
     {
         path: 'search',
         component: SearchComponent,
