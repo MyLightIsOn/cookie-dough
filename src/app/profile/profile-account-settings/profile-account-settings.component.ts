6e879d38c8c56968ab0295befcfea6a7779404e0
Lawrence Moore <lawrence2978@hotmail.com>
2018-03-24 10:43:26 -0400

    Added Account Settings styles

--- src/app/profile/profile-account-settings/profile-account-settings.component.ts
+++ src/app/profile/profile-account-settings/profile-account-settings.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../auth.service';
+import {AuthGuard} from '../../_guards/auth-guard.service';
 
 @Component({
     selector: 'app-profile-dashboard',
     templateUrl: './profile-account-settings.component.html',
     styleUrls: ['./profile-account-settings.component.css']
 })
-export class ProfileAccountSettingsComponent { }
+export class ProfileAccountSettingsComponent implements OnInit{
+    public user: object;
+    public email: string;
+    public company: string;
+    public accountType = 'Individual';
+    public userName: string;
+
+    constructor(public authGuard: AuthGuard, public authService: AuthService) {}
+
+    ngOnInit() {
+        this.user = this.authGuard.getSession();
+        this.email = this.user['user']['values']['field_19']['email'];
+        this.userName = this.user['user']['values']['field_50'];
+        if (this.user['user']['values']['field_51']) {
+            this.company = this.user['user']['values']['field_51'];
+            this.accountType = 'Business';
+        }
+    }
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,29 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { AuthGuard } from '../_guards/auth-guard.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-    public user: object;
-    public email: string;
-    public company: string;
-    public accountType = 'Individual';
-    public userName: string;
-
-    constructor(public authGuard: AuthGuard, public authService: AuthService) {}
-
-    ngOnInit() {
-        this.user = this.authGuard.getSession();
-        this.email = this.user['user']['values']['field_19']['email'];
-        this.userName = this.user['user']['values']['field_50'];
-        if (this.user['user']['values']['field_51']) {
-           this.company = this.user['user']['values']['field_51'];
-           this.accountType = 'Business';
-        }
-    }
-
-}
+export class ProfileComponent {}
