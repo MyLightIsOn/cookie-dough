d4bbe202c182d661c7ad663718e126c367b1b916
Lawrence Moore <lawrence2978@hotmail.com>
2018-03-30 14:40:44 -0400

    Adding config file

--- server/config.js
+++ server/config.js
@@ -1,13 +1,17 @@
-const apiIdHeader = process.env.API_ID_HEADER || 'API_ID_HEADER';
-const apiId = process.env.API_ID || '123456789';
-const apiKeyHeader = process.env.API_KEY_HEADER || 'API_KEY_HEADER';
-const apiKey = process.env.API_KEY || '987654321';
+const apiIdHeader = process.env.API_ID_HEADER || 'My-App-Id';
+const apiId = process.env.API_ID || '123456';
+const apiKeyHeader = process.env.API_KEY_HEADER || 'My-App-Key';
+const apiKey = process.env.API_KEY || '12345';
 const headers = {};
+const updateHeaders = {};
+
 headers[apiIdHeader] = apiId;
 headers[apiKeyHeader] = apiKey;
+updateHeaders[apiIdHeader] = apiId;
 
 module.exports = {
     baseURL : process.env.BASEURL || 'http://localhost:3000',
     headers: headers,
+    updatedHeaders: updateHeaders,
     appId : apiId
 };
